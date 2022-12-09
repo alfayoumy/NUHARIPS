da3f6692fc7db8938ea335146669fe981e97d66b
alfayoumy <amr.alfayoumi@gmail.com>
2022-12-09 20:15:29 +0200

    Update framework_app.py

--- framework_app.py
+++ framework_app.py
@@ -186,6 +186,7 @@ def record_event(event_ts, ips_pred, har_pred, event):
 def get_events():
     events = db.child("events").get()
     events_df = pd.DataFrame.from_dict(dict(events.val()), orient='index').reset_index(drop=True)
+    events_df['Event Timestamp'] =  pd.to_datetime(events_df['Event Timestamp'])
     events_df = events_df[['Event Timestamp', 'Location', 'Activity', 'Event']]
     events_df = events_df.sort_values('Event Timestamp', ascending=False)
     return events_df
