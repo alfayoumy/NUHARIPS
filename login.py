f73aea78236805d391bdcf1ec6cb680fd24ff7dd
alfayoumy <amr.alfayoumi@gmail.com>
2023-01-16 17:58:39 +0200

    Update login.py

--- login.py
+++ login.py
@@ -20,7 +20,6 @@ if authentication_status == True:
     st.success('Login successful.', icon = "✅")
     import app
     app.USERNAME = username
-    app()
     
 elif authentication_status == False:
     st.error('Username/password is incorrect')
