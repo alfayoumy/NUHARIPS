15196e50b783fa6fa7ae97c964a1b1ae14f2ce57
alfayoumy <amr.alfayoumi@gmail.com>
2023-02-01 18:19:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ import base64
 from email.message import EmailMessage
 import json
 import os
-import matplotlib.pyplot as plt
 
 #from contextlib import contextmanager, redirect_stdout
 #from io import StringIO
